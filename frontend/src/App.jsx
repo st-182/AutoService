85186f0eb0368de27b3540936c89d797b83f627e
st-182 <31894656+st-182@users.noreply.github.com>
2021-10-19 16:43:20 +0300

    initial strukture is done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Newsletter from "./components/Newsletter";
+import GlobalStyle from "./Global.style";
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <header className=""></header>
+      <main>
+        <Newsletter />
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Global.style.jsx
@@ -0,0 +1,9 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+/* style  */
+body{
+    
+}
+`;
+export default GlobalStyle;
