dab92bdfa5c932bef6ade3a7bb92bf360682ba13
st-182 <31894656+st-182@users.noreply.github.com>
2021-10-19 17:14:06 +0300

    initial content for pages added

---
+++ frontend/src/Screens/BlogScreens.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlogScreens = () => {
+  return (
+    <div>
+      <h1>BlogScreens</h1>
+    </div>
+  );
+};
+
+export default BlogScreens;
---
+++ frontend/src/Screens/ServicesScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ServicesScreen = () => {
+  return (
+    <div>
+      <h1>Services</h1>
+    </div>
+  );
+};
+
+export default ServicesScreen;
