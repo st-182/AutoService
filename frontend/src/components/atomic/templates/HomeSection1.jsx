a7a82d182da314a4626ecb6b6217f87cddc4b3cf
st-182 <31894656+st-182@users.noreply.github.com>
2021-10-20 13:25:01 +0300

    section1 done

--- frontend/src/components/atomic/templates/HomeSection1.jsx
+++ frontend/src/components/atomic/templates/HomeSection1.jsx
@@ -2,6 +2,53 @@ import React from "react";
 import Button from "../atoms/Button";
 import Container from "../atoms/Container";
 import clock from "../../../assets/icons/clock-svgrepo-com.svg";
+import styled from "styled-components";
+
+const StyledButton = styled(Button)`
+  position: absolute;
+  top: 25px;
+  left: 25px;
+  border-radius: 50px;
+  padding-left: 60px;
+  outline: 10px solid rgba(255, 255, 255, 0.5);
+  background: white;
+  color: black;
+  :before {
+    content: "24h";
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    width: 44px;
+    height: 44px;
+    background: #84e2c2;
+    border-radius: 50%;
+    color: white;
+  }
+`;
+const StyledButton2 = styled(StyledButton)`
+  top: initial;
+  left: initial;
+  bottom: 25px;
+  right: 25px;
+  :after {
+    content: "";
+    top: 50%;
+    right: 0;
+    position: absolute;
+
+    display: block;
+    transform: translate(-50%, -50%);
+    width: 12px;
+    height: 20px;
+    background: white;
+  }
+  :before {
+    content: "4.5/5";
+  }
+`;
 
 const HomeSection1 = () => {
   return (
@@ -29,7 +76,8 @@ const HomeSection1 = () => {
               src="https://www.autoservis-garant.cz/wp-content/uploads/2019/02/autoservis-garant-praha-strasnice00006-1024x683.jpg"
               alt="picture"
             />
-            <Button />
+            <StyledButton text="Quick Service" />
+            <StyledButton2 text="Rating: ⭐⭐⭐⭐⭐" />
           </div>
         </div>
       </Container>
