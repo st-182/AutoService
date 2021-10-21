8048f9643428b1f1a3bccd5496d698be62f6641e
st-182 <31894656+st-182@users.noreply.github.com>
2021-10-21 10:08:00 +0300

    Section 3 is ready for review

--- frontend/src/components/atomic/templates/HomeSection3.jsx
+++ frontend/src/components/atomic/templates/HomeSection3.jsx
@@ -3,6 +3,9 @@ import Button from "../atoms/Button";
 import Container from "../atoms/Container";
 import clock from "../../../assets/icons/clock-svgrepo-com.svg";
 import styled from "styled-components";
+import one from "../../../assets/icons/one-svgrepo-com.svg";
+import two from "../../../assets/icons/two-svgrepo-com.svg";
+import three from "../../../assets/icons/three-svgrepo-com.svg";
 
 const StyledButton = styled(Button)`
   position: absolute;
@@ -28,25 +31,18 @@ const StyledButton = styled(Button)`
     color: white;
   }
 `;
-const StyledButton2 = styled(StyledButton)`
-  top: initial;
-  left: initial;
-  bottom: 25px;
-  right: 25px;
+const StyledDiv = styled.div`
   :after {
     content: "";
-    top: 50%;
-    right: 0;
+    top: 30px;
+    left: -18px;
     position: absolute;
-
     display: block;
-    transform: translate(-50%, -50%);
-    width: 12px;
-    height: 20px;
-    background: white;
-  }
-  :before {
-    content: "4.5/5";
+    /* transform: translate(-50%, -50%); */
+    width: 100%;
+    height: 80%;
+    /* background: #fc0000; */
+    border-left: 2px dotted orange;
   }
 `;
 
@@ -59,13 +55,53 @@ const HomeSection3 = () => {
             <h2 className="my-5 font-bold text-4xl">
               We follow a clear process to help you out.
             </h2>
-            <p>
+            <p className="my-5 ">
               Through True Rich Attended does no end it his mother since real
               had half every him case in packages enquire
             </p>
             <Button text="Learn More" />
           </div>
-          <div className="relative"></div>
+          <div className="relative">
+            {/*//! Process 1 */}
+            <StyledDiv className="relative my-7 ml-8">
+              <img
+                src={one}
+                alt="clock"
+                className="absolute top-3 left-0 w-8 h-8 transform -translate-x-full -translate-y-1/2 "
+              />
+              <h2 className=" font-bold my-1">Get a Quote </h2>
+              <p className="text-gray-600">
+                Through True Rich Attended does no end it his mother since real
+                had half every.
+              </p>
+            </StyledDiv>
+            {/*//! Process 2 */}
+            <StyledDiv className="relative my-7 ml-8">
+              <img
+                src={two}
+                alt="clock"
+                className="absolute top-3 left-0 w-8 h-8 transform -translate-x-full -translate-y-1/2 "
+              />
+              <h2 className=" font-bold my-1">Book an Appointment </h2>
+              <p className="text-gray-600">
+                Through True Rich Attended does no end it his mother since real
+                had half every.
+              </p>
+            </StyledDiv>
+            {/*//! Process 3 */}
+            <div className="relative my-7 ml-8">
+              <img
+                src={three}
+                alt="clock"
+                className="absolute top-3 left-0 w-8 h-8 transform -translate-x-full -translate-y-1/2 "
+              />
+              <h2 className=" font-bold my-1">Get your Service Done </h2>
+              <p className="text-gray-600">
+                Through True Rich Attended does no end it his mother since real
+                had half every.
+              </p>
+            </div>
+          </div>
         </div>
       </Container>
     </section>
