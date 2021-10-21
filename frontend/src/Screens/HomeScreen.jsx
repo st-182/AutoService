b3b3e23a1d66681f3dc20397817f1d4437a18650
st-182 <31894656+st-182@users.noreply.github.com>
2021-10-21 09:49:20 +0300

    small

--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HomeSection1 from "../components/atomic/templates/HomeSection1";
+import HomeSection3 from "../components/atomic/templates/HomeSection3";
 
 const HomeScreen = () => {
   return (
     <section>
       <h1>Home</h1>
       <HomeSection1 />
+      <HomeSection3 />
     </section>
   );
 };
